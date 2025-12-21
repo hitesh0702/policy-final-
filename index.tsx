23fc1fc047f4f4fe7e8af6c3ab473babcb446520
Hitesh <73782583+hitesh0702@users.noreply.github.com>
2025-12-21 22:09:17 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider } from './hooks/useTheme';
+import { supabase } from "./lib/supabase";
 
+supabase.auth.onAuthStateChange((event) => {
+  if (event === "SIGNED_IN") {
+    window.location.href = "/";
+  }
+});
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
