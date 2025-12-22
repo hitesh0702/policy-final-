f95b0af6f752b19cb680a7afcfd5e5ab0b50ad5e
Hitesh <73782583+hitesh0702@users.noreply.github.com>
2025-12-22 13:30:22 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,28 +1,12 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-import { ThemeProvider } from './hooks/useTheme';
-import { supabase } from "./lib/supabase";
+import App from "./App";
 
-supabase.auth.onAuthStateChange((event) => {
-  if (event === "SIGNED_IN") {
-    window.location.href = "/";
-  }
-});
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
